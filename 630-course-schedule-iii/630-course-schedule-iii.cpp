66566a8c721ffd4968195c55ad89cd6400a3fc67
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-06-24 04:09:37 +0530

    Time: 399 ms (39.29%), Space: 63.5 MB (30.03%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,10 +1,3 @@
-class Comparator {
-    public :
-    bool operator()(vector<int> &c1, vector<int> &c2) {
-        return c1[0] < c2[0];    
-    }  
-};
-
 class Solution {
 public:
     static bool compare(vector<int> &c1, vector<int> &c2) {
@@ -12,21 +5,19 @@ public:
     }  
     int scheduleCourse(vector<vector<int>>& courses) {
         int day = 0;
-        priority_queue<vector<int>, vector<vector<int>>, Comparator> pq;
+        priority_queue<int> pq;
         sort(courses.begin(), courses.end(), compare);
+        
         for(auto c : courses) {
             if(day + c[0] <= c[1]) {
                 day += c[0];
-                pq.push(c);
+                pq.push(c[0]);
             }
-            else if(!pq.empty()){
-                auto t = pq.top();
-                if(t[0] > c[0]) {
-                    pq.pop();
-                    pq.push(c);
-                    day -= t[0];
-                    day += c[0];
-                }
+            else if(!pq.empty() && pq.top() > c[0]){
+                day -= pq.top();
+                pq.pop();
+                pq.push(c[0]);
+                day += c[0];
             }
         }
         return pq.size();
