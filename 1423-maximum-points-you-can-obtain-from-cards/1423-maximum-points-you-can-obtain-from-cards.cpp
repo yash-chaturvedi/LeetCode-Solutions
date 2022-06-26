403c78b66900058bfae0e3087cd52059cba3c0b2
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-06-27 02:04:46 +0530

    Time: 74 ms (75.34%), Space: 42.5 MB (48.00%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,22 +1,41 @@
+
 class Solution {
-public:
+    public :
+    
     int maxScore(vector<int>& cardPoints, int k) {
-        int n = cardPoints.size(), ans = 0;
-        for(int i=1; i<n; i++) {
-            cardPoints[i] += cardPoints[i-1];
+        int n = cardPoints.size(), lSum = 0;
+        for(int i = 0; i < k; ++i){
+            lSum += cardPoints[i];
         }
-        
-        for(int i=0; i<=k; i++) {
-            int left = i > 0 ? cardPoints[i-1] : 0;
-            int right = k-i > 0 
-                ? cardPoints[n-1] - ((n-1-(k-i )>= 0) ? cardPoints[n-1-(k-i)] : 0 )
-                : 0;
-            // cout<<left<<" "<<right<<" "<<cardPoints[n-1]<<" "<<cardPoints[n-1-k-i]<<endl;
-            ans = max(ans, left + right);
+        int ans = lSum;
+        for(int rSum = 0, i = 0; i < k; ++i){
+            rSum += cardPoints[n-1-i];
+            lSum -= cardPoints[k-1-i];
+            ans = max(ans,lSum+rSum);
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int maxScore(vector<int>& cardPoints, int k) {
+//         int n = cardPoints.size(), ans = 0;
+//         for(int i=1; i<n; i++) {
+//             cardPoints[i] += cardPoints[i-1];
+//         }
+        
+//         for(int i=0; i<=k; i++) {
+//             int left = i > 0 ? cardPoints[i-1] : 0;
+//             int right = k-i > 0 
+//                 ? cardPoints[n-1] - ((n-1-(k-i )>= 0) ? cardPoints[n-1-(k-i)] : 0 )
+//                 : 0;
+//             // cout<<left<<" "<<right<<" "<<cardPoints[n-1]<<" "<<cardPoints[n-1-k-i]<<endl;
+//             ans = max(ans, left + right);
+//         }
+//         return ans;
+//     }
+// };
 
 // class Solution {
 // public:
