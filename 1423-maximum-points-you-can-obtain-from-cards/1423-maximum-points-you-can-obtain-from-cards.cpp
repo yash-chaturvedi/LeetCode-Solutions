450743527fc93c4e4398db19dd326cd790c8c520
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-06-27 01:59:21 +0530

    Time: 120 ms (17.34%), Space: 42.5 MB (48.00%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size(), ans = 0;
+        for(int i=1; i<n; i++) {
+            cardPoints[i] += cardPoints[i-1];
+        }
+        
+        for(int i=0; i<=k; i++) {
+            int left = i > 0 ? cardPoints[i-1] : 0;
+            int right = k-i > 0 
+                ? cardPoints[n-1] - ((n-1-(k-i )>= 0) ? cardPoints[n-1-(k-i)] : 0 )
+                : 0;
+            // cout<<left<<" "<<right<<" "<<cardPoints[n-1]<<" "<<cardPoints[n-1-k-i]<<endl;
+            ans = max(ans, left + right);
+        }
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
+    
+//     int helper(int i, int j, vector<int> &cardPoints, int k, vector<vector<vector<int>>> &dp) {
+//         if(i > j) return 0;
+//         if(k == 0) return 0;
+//         if(dp[i][j][k] != -1) return dp[i][j][k];
+//         int left = cardPoints[i] + helper(i+1, j, cardPoints, k-1, dp);
+//         int right = cardPoints[j] + helper(i, j-1, cardPoints, k-1, dp);
+        
+//         return dp[i][j][k] = max(left, right);
+//     }
+    
+//     int maxScore(vector<int>& cardPoints, int k) {
+//         int n = cardPoints.size();
+//         vector<vector<vector<int>>> dp(n, vector<vector<int>> (n, vector<int> (n+1, -1)));
+//         return helper(0, n-1, cardPoints, k, dp);
+//     }
+// };
