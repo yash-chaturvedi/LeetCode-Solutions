d81c6cd682a375d5866937dd57a38926f5773f77
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-06-29 01:13:51 +0530

    Time: 56 ms (89.70%), Space: 17.1 MB (98.17%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,24 +2,47 @@ class Solution {
 public:
     int minDeletions(string s) {
         int n = s.size(), ans = 0;
-        vector<int> freq(26, 0);
-        vector<bool> freqUsed(n+1, false);
+        vector<int> charFreq(26, 0);
         
         for(char c : s) {
-            freq[c - 'a']++;
+            charFreq[c - 'a']++;
         }
         
-        for(int i=0; i<26; i++) {
-            if(freq[i] > 0) {
-                int cnt = 0;
-                while(freq[i] > 0 && freqUsed[freq[i]]) {
-                    cnt++;
-                    freq[i]--;
-                }
-                if(freq[i] >= 0) freqUsed[freq[i]] = true;
-                ans += cnt;
+        sort(charFreq.begin(), charFreq.end());
+        
+        for(int i=24; i>=0; i--) {
+            int prevFreq = charFreq[i];
+            if(charFreq[i] >= charFreq[i+1]) {
+                charFreq[i] = max(0, charFreq[i+1] - 1);
             }
+            ans += prevFreq - charFreq[i];
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int minDeletions(string s) {
+//         int n = s.size(), ans = 0;
+//         vector<int> freq(26, 0);
+//         vector<bool> freqUsed(n+1, false);
+        
+//         for(char c : s) {
+//             freq[c - 'a']++;
+//         }
+        
+//         for(int i=0; i<26; i++) {
+//             if(freq[i] > 0) {
+//                 int cnt = 0;
+//                 while(freq[i] > 0 && freqUsed[freq[i]]) {
+//                     cnt++;
+//                     freq[i]--;
+//                 }
+//                 if(freq[i] >= 0) freqUsed[freq[i]] = true;
+//                 ans += cnt;
+//             }
+//         }
+//         return ans;
+//     }
+// };
