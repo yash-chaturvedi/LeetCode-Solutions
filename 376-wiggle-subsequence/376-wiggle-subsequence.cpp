65e09ce16e8bdaa010a6f90cbf5b67791253d2c2
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-07-04 02:38:03 +0530

    Time: 3 ms (69.28%), Space: 7.6 MB (7.35%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -3,21 +3,44 @@ public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
         if(n < 2) return n;
-        // We have two states :
-        // 0 -> cur element is greater than prev
-        // 1 -> cur element is smaller than prev
         vector<vector<int>> dp(n+1, vector<int> (2, 0));
-        for(int i=0; i<n; i++) {
-            dp[i][0] = dp[i][1] = 1;
-            for(int j=0; j<i; j++) {
-                if(nums[i] > nums[j]) {
-                    dp[i][0] = max(dp[i][0], 1 + dp[j][1]);
-                }
-                else if(nums[i] < nums[j]) {
-                    dp[i][1] = max(dp[i][1], 1 + dp[j][0]);
-                }
+        dp[0][0] = dp[0][1] = 1;
+        for(int i=1; i<n; i++) {
+            
+            dp[i][0] = dp[i-1][0];
+            dp[i][1] = dp[i-1][1];
+            
+            if(nums[i] > nums[i-1]) {
+                dp[i][0] = 1 + dp[i-1][1];
+            }
+            else if(nums[i] < nums[i-1]) {
+                dp[i][1] = 1 + dp[i-1][0];
             }
         }
         return max(dp[n-1][0], dp[n-1][1]);
     }
 };
+
+// class Solution {
+// public:
+//     int wiggleMaxLength(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n < 2) return n;
+//         // We have two states :
+//         // 0 -> cur element is greater than prev
+//         // 1 -> cur element is smaller than prev
+//         vector<vector<int>> dp(n, vector<int> (2, 0));
+//         for(int i=0; i<n; i++) {
+//             dp[i][0] = dp[i][1] = 1;
+//             for(int j=0; j<i; j++) {
+//                 if(nums[i] > nums[j]) {
+//                     dp[i][0] = max(dp[i][0], 1 + dp[j][1]);
+//                 }
+//                 else if(nums[i] < nums[j]) {
+//                     dp[i][1] = max(dp[i][1], 1 + dp[j][0]);
+//                 }
+//             }
+//         }
+//         return max(dp[n-1][0], dp[n-1][1]);
+//     }
+// };
