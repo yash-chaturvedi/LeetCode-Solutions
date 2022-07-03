eca17d8b6454af2b052c6977fabbbb15893e0a9f
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-07-04 02:41:16 +0530

    Time: 4 ms (56.18%), Space: 7 MB (95.54%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -3,23 +3,42 @@ public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
         if(n < 2) return n;
-        vector<vector<int>> dp(n+1, vector<int> (2, 0));
-        dp[0][0] = dp[0][1] = 1;
+        int up = 1, down = 1;
+        
         for(int i=1; i<n; i++) {
-            
-            dp[i][0] = dp[i-1][0];
-            dp[i][1] = dp[i-1][1];
-            
             if(nums[i] > nums[i-1]) {
-                dp[i][0] = 1 + dp[i-1][1];
+                up = 1 + down;
             }
             else if(nums[i] < nums[i-1]) {
-                dp[i][1] = 1 + dp[i-1][0];
+                down = 1 + up;
             }
         }
-        return max(dp[n-1][0], dp[n-1][1]);
+        return max(up, down);
     }
 };
+
+// class Solution {
+// public:
+//     int wiggleMaxLength(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n < 2) return n;
+//         vector<vector<int>> dp(n+1, vector<int> (2, 0));
+//         dp[0][0] = dp[0][1] = 1;
+//         for(int i=1; i<n; i++) {
+            
+//             dp[i][0] = dp[i-1][0];
+//             dp[i][1] = dp[i-1][1];
+            
+//             if(nums[i] > nums[i-1]) {
+//                 dp[i][0] = 1 + dp[i-1][1];
+//             }
+//             else if(nums[i] < nums[i-1]) {
+//                 dp[i][1] = 1 + dp[i-1][0];
+//             }
+//         }
+//         return max(dp[n-1][0], dp[n-1][1]);
+//     }
+// };
 
 // class Solution {
 // public:
