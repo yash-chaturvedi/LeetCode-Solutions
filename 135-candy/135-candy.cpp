8981184e40856c384d391f1b1a32c21e461864ae
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-07-06 00:50:13 +0530

    Time: 16 ms (98.41%), Space: 17.2 MB (96.64%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,21 +2,51 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> candies(n, 1);
+        
+        if(n < 2) return n;
+        
+        int ans = 1, up = 0, down = 0, peak = 0;
+        
         for(int i=1; i<n; i++) {
-            if(ratings[i] > ratings[i-1]) {
-                candies[i] = candies[i-1] + 1;
+            int slope = ratings[i] - ratings[i-1];
+            if(slope > 0) {
+                down = 0;
+                peak = ++up;
+                ans += 1 + up;
             }
-        }
-        for(int i=n-2; i>=0; i--) {
-            if(ratings[i] > ratings[i+1]) {
-                candies[i] = max(candies[i], candies[i+1] + 1);
+            else if(slope == 0) {
+                up = down = peak = 0;
+                ans += 1;
             }
-        }
-        int ans = 0;
-        for(int i=0; i<n; i++) {
-            ans += candies[i];
+            else {
+                up = 0;
+                ans += ++down;
+                if(down > peak) ans++;
+            }
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int candy(vector<int>& ratings) {
+//         int n = ratings.size();
+//         vector<int> candies(n, 1);
+//         for(int i=1; i<n; i++) {
+//             if(ratings[i] > ratings[i-1]) {
+//                 candies[i] = candies[i-1] + 1;
+//             }
+//         }
+//         for(int i=n-2; i>=0; i--) {
+//             if(ratings[i] > ratings[i+1]) {
+//                 candies[i] = max(candies[i], candies[i+1] + 1);
+//             }
+//         }
+//         int ans = 0;
+//         for(int i=0; i<n; i++) {
+//             ans += candies[i];
+//         }
+//         return ans;
+//     }
+// };
