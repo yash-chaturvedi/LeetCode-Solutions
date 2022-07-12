f852bd9e3f078324db6ab5de23eb31c69bc2c800
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-07-13 01:13:59 +0530

    Time: 2979 ms (5.02%), Space: 10 MB (67.39%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,50 @@
+// class Solution {
+// public:
+    
+//     bool helper(vector<int>& matchsticks, int n, int mask, int sidesDone) {
+        
+//     }
+    
+//     bool makesquare(vector<int>& matchsticks) {
+//         int n = matchsticks.size();
+//         int k = 0;
+//         for(int l : matchsticks) k+=l;
+//         if(k % 4 != 0) return false;
+//         k /= 4;
+//         return helper(matchsticks, n, );
+//     }
+// };
+
+class Solution {
+public:
+    
+    bool helper(vector<int>& matchsticks, int n, int i, int a, int b, int c, int d, int k) {
+        if(i==n) return (a==b && b==c && c==d && d==k);
+        
+        bool case1=false, case2=false, case3=false, case4=false;
+        
+        if(a + matchsticks[i] <= k) 
+            case1 = helper(matchsticks, n, i+1, a + matchsticks[i], b, c, d, k);
+        
+        if(b + matchsticks[i] <= k) 
+            case2 = helper(matchsticks, n, i+1, a, b + matchsticks[i], c, d, k);
+        
+        if(c + matchsticks[i] <= k) 
+            case3 = helper(matchsticks, n, i+1, a, b, c + matchsticks[i], d, k);
+        
+        if(d + matchsticks[i] <= k) 
+            case4 = helper(matchsticks, n, i+1, a, b, c, d + matchsticks[i], k);
+        
+        return case1 || case2 || case3 || case4;
+    }
+    
+    bool makesquare(vector<int>& matchsticks) {
+        int n = matchsticks.size();
+        int k = 0;
+        for(int l : matchsticks) k+=l;
+        if(k % 4 != 0) return false;
+        k /= 4;
+        sort(matchsticks.begin(), matchsticks.end(), greater<>());
+        return helper(matchsticks, n, 0, 0, 0, 0, 0, k);
+    }
+};
