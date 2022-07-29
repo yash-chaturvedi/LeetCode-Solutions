23918c6b1523352453ca22e3a775ab8877d4a1a2
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-07-30 01:46:04 +0530

    Time: 3 ms (89.85%), Space: 8.2 MB (84.30%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,38 +1,62 @@
 class Solution {
 public:
-    bool matches(string &s, string &p) {
-        int n = s.size(), m = p.size();
-        
-        if(n != m) return false;
-        
-        unordered_map<char, char> mp1, mp2;
-        
-        for(int i=0; i<n; i++) {
-            bool sCharFound = mp1.find(s[i]) != mp1.end();
-            bool pCharFound = mp2.find(p[i]) != mp2.end();
-            
-            if(sCharFound && pCharFound) {
-                if(mp1[s[i]] != p[i] || mp2[p[i]] != s[i]) return false;
-            }
-            else if(sCharFound) {
-                return false;
-            }
-            else if(pCharFound) {
-                return false;
-            }
-            else {
-                mp1[s[i]] = p[i];
-                mp2[p[i]] = s[i];
-            }
+    string transform(string s) {
+        unordered_map<char, int> mp;
+        int delta = 0;
+        for(char c : s) {
+            if(mp.find(c) == mp.end()) mp[c] = delta++;
         }
-        return true;
+        for(int i=0; i<s.size(); i++) {
+            s[i] = 'a' + mp[s[i]];
+        }
+        return s;
     }
     
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string> ans;
+        pattern = transform(pattern);
         for(string s : words) {
-            if(matches(s, pattern)) ans.push_back(s);
+            if(transform(s) == pattern) ans.push_back(s);
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     bool matches(string &s, string &p) {
+//         int n = s.size(), m = p.size();
+        
+//         if(n != m) return false;
+        
+//         unordered_map<char, char> mp1, mp2;
+        
+//         for(int i=0; i<n; i++) {
+//             bool sCharFound = mp1.find(s[i]) != mp1.end();
+//             bool pCharFound = mp2.find(p[i]) != mp2.end();
+            
+//             if(sCharFound && pCharFound) {
+//                 if(mp1[s[i]] != p[i] || mp2[p[i]] != s[i]) return false;
+//             }
+//             else if(sCharFound) {
+//                 return false;
+//             }
+//             else if(pCharFound) {
+//                 return false;
+//             }
+//             else {
+//                 mp1[s[i]] = p[i];
+//                 mp2[p[i]] = s[i];
+//             }
+//         }
+//         return true;
+//     }
+    
+//     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+//         vector<string> ans;
+//         for(string s : words) {
+//             if(matches(s, pattern)) ans.push_back(s);
+//         }
+//         return ans;
+//     }
+// };
