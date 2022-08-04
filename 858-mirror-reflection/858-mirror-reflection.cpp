c0ecfec3116c2a7aa414b0ce44a3e6e4d062a9d4
Yash Chaturvedi <51001010+yash-chaturvedi@users.noreply.github.com>
2022-08-05 03:51:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-        while(p%2 == 0 && q%2 == 0) {
-            p/=2;
-            q/=2;
-        }
+        // while(p%2 == 0 && q%2 == 0) {
+        //     p/=2;
+        //     q/=2;
+        // }
+        
+        int g = gcd(p, q);
+        p /= g;
+        q /= g;
+        
         int numReflections = p, numRooms = q;
         
         if(numReflections % 2 == 0) return 2;
